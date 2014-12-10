7ade3dbc5fac2007c7b6532b94e79a98f6df0e02
cmnjcs <cmn.jcs@gmail.com>
2014-12-10 16:57:43 -0500

    The server now handles each request correctly
    Instead of overwriting variables, using Meteor.bindEnvironment() allows
    us to keep each incoming request from interfering with each other.

---
+++ README.md
@@ -0,0 +1,3 @@
+A basic example of the application of Futures in a Meteor application. Some good reading:
+
+https://meteorhacks.com/fibers-eventloop-and-meteor.html
--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 var Future = Npm.require('fibers/future');
 var device;
-var answer;
+// var answer;
 
 Meteor.startup( function() {
   Spark.login({accessToken: "e2718dc81e1fd936bb4f6e8f6b2b9fbac4ebb2b0"});
@@ -8,25 +8,63 @@ Meteor.startup( function() {
     if(err) {
       console.log(err);
     } else {
-      device = dev; 
+      // console.log('device found');
+      device = dev;
     }
   });
 });
 
 Meteor.methods({
   'buttonClick': function() {
-    var toReturn;
-    device.callFunction('getCounter', '1', function(err, data) {
-      if(err) {
-        console.log(err);
-      } else {
-        answer = data.return_value;
-        toReturn = data.return_value;
+    console.log('button clicked');
+    // console.log(future);
+    var func = Meteor.bindEnvironment(function() {
+      // var a;
+      var toReturn;
+      var future = new Future();
+      var rand = Math.random();
+      console.log("rand is", rand);
+      if(rand>=.9) {
+        pausecomp(3000);
+        // var x = 0;
+        // while(x<1000000000) {
+        //   x+=1;
+        // }
       }
-    })
-    return toReturn;
+
+      device.callFunction('getCounter', '1', function(err, data) {
+        if(err) {
+          console.log(err);
+          future.throw( new Meteor.Error(error.type, error.message) );
+        } else {
+          // answer = data.return_value;
+          toReturn = data.return_value;
+          a = data.return_value;
+          console.log('about to return:', toReturn);
+          console.log("ending rand is", rand);
+          future.return(toReturn);
+        }
+      });
+      // console.log('waiting');
+      // console.log('about to return: ' + a);
+      return future.wait();
+      // return toReturn;
+      // console.log(future);
+    });
+    // return toReturn;
+    var ret = func();
+    // console.log("returning");
+    return ret;
   },
   'buttonClick2': function() {
     return answer;
   }
 })
+
+function pausecomp(millis)
+ {
+  var date = new Date();
+  var curDate = null;
+  do { curDate = new Date(); }
+  while(curDate-date < millis);
+}
